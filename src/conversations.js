00b36c00dcd7a320d15ea3a7ac3a2d0c703e1af7
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-04-19 11:13:30 -0400

    Update conversations.js

--- src/conversations.js
+++ src/conversations.js
@@ -276,17 +276,6 @@ const nothingConversations = [
 	'We do a minuscule amount of tomfoolery',
 	'hi',
 	'hey',
-	'hello',
-	'?ban',
-	'>level',
-	'why is everyone afk in the vc?',
-	'a',
-	'does anyone still play Chemcremental?',
-	'anyone remember plasma clicker?',
-	'lol',
-	'lmao',
-	'im going to do a P2W Eternal speedrun!',
-	'5 hours'
 ].reduce((acc, curr, index) => {
 	acc['nothing' + index] = typeof curr === 'string' ? singleMessage(curr) : curr;
 	return acc;
