cf822726cf1112539e4090eb30c90cd3b1c567e5
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-04-21 14:30:28 -0500

    Another One

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="upgradesParent">
     <p class="upgradesHeader">Upgrades Store</p>
-    <p class="upgradesCategoryHeader">Upgrade Category</p>
+    <p class="upgradesCategoryHeader" style="color:var(--color-influence);">Influential Upgrades</p>
+    <div class="upgradesGrid">
+      <button v-on:click="logSomething('test1')">Purchase Upgrade 1</button>
+      <button v-on:click="logSomething('test2')">Purchase Upgrade 2</button>
+    </div>
+    <p class="upgradesCategoryHeader">Upgrade Category 2</p>
     <div class="upgradesGrid">
       <button v-on:click="logSomething('test1')">Purchase Upgrade 1</button>
       <button v-on:click="logSomething('test2')">Purchase Upgrade 2</button>
@@ -18,21 +23,23 @@ name: "store",
     }
   }
 }
+//TODO Code to set Upgrade Color based on costs and currency
 </script>
 
 <style scoped>
   .upgradesParent {
     justify-content: flex-start;
-    align-items: center;
+    flex-start: left;
     height: calc(100vh - 3em);
     display: flex;
     background-color: var(--background-secondary);
     flex-direction: column;
   }
   .upgradesGrid {
-    display: grid;
+    display: flex;
     grid-template-columns: repeat(2, 1fr);
     grid-auto-rows: 1fr;
+    margin-bottom: 2em;
   }
   .upgradesHeader {
     font-size: 2em;
@@ -41,6 +48,6 @@ name: "store",
   }
   .upgradesCategoryHeader {
     font-size: 1.5em;
-    margin-bottom: 1em;
+    margin-bottom: 0.5em;
   }
 </style>
