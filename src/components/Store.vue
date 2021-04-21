41d7c4fee6490d4fcf4e89072e0b88b63ecf745c
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-04-21 13:49:22 -0500

    Added some new CSS classes

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="upgradesParent">
-    <span>Upgrades Store</span>
+    <span class="upgradesHeader">Upgrades Store</span>
+    <span class="upgradesCategoryHeader">Upgrade Category</span>
     <div class="upgradesGrid">
-
       <button v-on:click="logSomething('test1')">Purchase Upgrade 1</button>
       <button v-on:click="logSomething('test2')">Purchase Upgrade 2</button>
     </div>
@@ -22,16 +22,25 @@ name: "store",
 
 <style scoped>
   .upgradesParent {
-    justify-content: space-around;
+    justify-content: flex-start;
     align-items: center;
     height: calc(100vh - 3em);
     display: flex;
     background-color: var(--background-secondary);
-    flex-direction: column
+    flex-direction: column;
   }
   .upgradesGrid {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
     grid-auto-rows: 1fr;
+  }
+  .upgradesHeader {
+    font-size: 4em;
+    margin-top: 2em;
+    margin-bottom: 2em;
+  }
+  .upgradesCategoryHeader {
+    font-size: 2.5em;
+    margin-bottom: 2em;
   }
 </style>
